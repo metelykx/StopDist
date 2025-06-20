1e204066d93c28501e2f4f705ae41c9c43f0e551
metelykx <ivaschenkoodenys@gmail.com>
2025-06-20 11:10:52 +0300

    New file

---
+++ StopDist/View/StartView.swift
@@ -0,0 +1,18 @@
+//
+//  StartView.swift
+//  StopDist
+//
+//  Created by Denis Ivaschenko on 20.06.2025.
+//
+
+import SwiftUI
+
+struct StartView: View {
+    var body: some View {
+        Text("ded")
+    }
+}
+
+#Preview {
+    StartView()
+}
